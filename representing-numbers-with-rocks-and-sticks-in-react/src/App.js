5035293ac6c1fc6c94886bffd8cde9c17568c977
Paul Ness <paul.ness@haymarketmedia.com>
2017-11-07 21:26:35 -0500

    Update App.js

--- representing-numbers-with-rocks-and-sticks-in-react/src/App.js
+++ representing-numbers-with-rocks-and-sticks-in-react/src/App.js
@@ -145,7 +145,6 @@ class App extends Component {
   render() {
     return (
       <div className="App">
-        <h1>Any number represented in rock formation</h1>
         <fieldset>
           <legend>
             Key
